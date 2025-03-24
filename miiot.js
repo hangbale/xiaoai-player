5219b98258729eb6b3245041072fabcd6cabf50b
hangbale <571312070@qq.com>
2025-03-24 17:04:27 +0800

    优化播放问题

--- miiot.js
+++ miiot.js
@@ -1,5 +1,15 @@
 import { getMiIOT, getMiNA } from "mi-service-lite";
 
+const USE_PLAY_MUSIC_API = [
+    "LX04",
+    "L05B",
+    "L05C",
+    "L06",
+    "L06A",
+    "X08A",
+    "X10A",
+]
+
 export default {
     clinet: null,
     userInfo: {},
@@ -69,12 +79,56 @@ export default {
     getSpeakerPlayStatus: async function() {
         return this.getProperty(3, 1)
     },
+    // 部分设备的音乐播放api不一样
+    // https://github.com/yihong0618/MiService/blob/57fdc6f18d214c3a6e09b684b80a81f00066ebf6/miservice/minaservice.py#L163
+    // mi-service-lite库中写死的"mediaplayer", "player_play_url"
+    // 小爱音箱paly增强版要用 "mediaplayer", "player_play_music",
     speakerPlayOnlineMusic: async function(url) {
         if(!this.minaClient) {
             await this.initMina()
         }
-        this.minaClient.play({
-            url
-        })
+        if(USE_PLAY_MUSIC_API.includes(this.minaClient.account.device.hardware)) {
+            console.log('ffd')
+            console.log(this.minaClient.ubus)
+            let d = genMusicPostData(url)
+            console.log(d)
+            this.minaClient.ubus('mediaplayer', 'player_play_music', d)
+        } else {
+            this.minaClient.play({
+                url
+            })
+        }
+    }
+}
+
+function genMusicPostData(url) {
+    return {
+        startaudioid: '1582971365183456177',
+        music: {
+            payload: {
+                audio_type: 'MUSIC', // MUSIC 灯会亮
+                audio_items: [
+                    {
+                        item_id: {
+                            audio_id: '1582971365183456177',
+                            cp: {
+                                album_id: -1,
+                                episode_index: 0,
+                                id: '355454500',
+                                name: 'xiaowei',
+                            },
+                        },
+                        stream: {url: url},
+                    }
+                ],
+                list_params: {
+                    listId: -1,
+                    loadmore_offset: 0,
+                    origin: 'xiaowei',
+                    type: 'MUSIC',
+                },
+            },
+            play_behavior: 'REPLACE_ALL',
+        }
     }
 }
